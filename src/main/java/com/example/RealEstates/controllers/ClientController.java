77e1348d6f2d39398ea2795f3906668405ef30e5
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-05 14:41:43 +0200

    add clientController + clientService

--- src/main/java/com/example/RealEstates/controllers/ClientController.java
+++ src/main/java/com/example/RealEstates/controllers/ClientController.java
@@ -1,4 +1,48 @@
 package com.example.RealEstates.controllers;
 
+import com.example.RealEstates.models.Client;
+import com.example.RealEstates.services.ClientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/client")
 public class ClientController {
+
+    private ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("") //localhost:9999/client
+    public List<Client> getClients() {
+        return clientService.getAll();
+    }
+
+    @GetMapping("/{id}") //localhost:9999/client/XX <- you fill in XX
+    public Client getClient(@PathVariable("id") int id) {
+        return clientService.get(id);
+    }
+
+    @PostMapping("/create") //localhost:9999/client/create
+    @ResponseStatus(HttpStatus.CREATED) //gives this status
+    public Client createClient(@RequestBody Client client) {
+        return clientService.create(client);
+    }
+
+    @PutMapping("/{id}") //localhost:9999/client/XX <- you fill in XX
+    public Client updateClient(@PathVariable("id") int id, @RequestBody Client client) {
+        return clientService.update(id, client);
+    }
+
+    @DeleteMapping("/{id}") //localhost:9999/client/XX <- you fill in XX
+    public String deleteClient(@PathVariable("id") int id) {
+        clientService.delete(id);
+        return "OK - client deleted.";
+
+    }
 }
+
--- src/main/java/com/example/RealEstates/services/ClientService.java
+++ src/main/java/com/example/RealEstates/services/ClientService.java
@@ -1,4 +1,36 @@
 package com.example.RealEstates.services;
 
+import com.example.RealEstates.models.Client;
+import com.example.RealEstates.repositories.ClientRepository;
+
+import java.util.List;
+
 public class ClientService {
+
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> getAll() {
+        return clientRepository.getAll();
+    }
+
+    public Client get(int id) {
+        return clientRepository.findbyId(id);
+    }
+
+    public Client create(Client client) {
+        return clientRepository.add(client);
+    }
+
+
+    public Client update(int id, Client client) {
+        return clientRepository.updateById(id, client);
+    }
+
+    public void delete(int id) {
+        clientRepository.deleteById(id);
+    }
 }
