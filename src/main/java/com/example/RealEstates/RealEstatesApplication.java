a4e2d882166bd7233e8449ff13507e6ead5e215a
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-05 12:34:58 +0200

    initial commit

---
+++ src/main/java/com/example/RealEstates/RealEstatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.RealEstates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstatesApplication.class, args);
+	}
+
+}
