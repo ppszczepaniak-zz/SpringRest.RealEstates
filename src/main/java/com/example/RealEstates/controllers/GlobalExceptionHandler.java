9d3282ac74f8df54f91a4f717048a42dc4e4ae36
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-05 15:25:43 +0200

    add globalExceptionHandler.java

--- src/main/java/com/example/RealEstates/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/example/RealEstates/controllers/GlobalExceptionHandler.java
@@ -1,4 +1,29 @@
 package com.example.RealEstates.controllers;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    //TODO obczaic to
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ResponseBody
+    protected ResponseEntity<Object> handleIllegalArgumentException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    @ResponseBody
+    protected ResponseEntity<Object> handletException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
 }
